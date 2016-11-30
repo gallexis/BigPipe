5249c81e16ca7d745f299295b416f7dbcbfd1f37
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-11-30 16:31:10 +0100

    removed useless files

--- BigPipe/include/pagelets.php
+++ BigPipe/include/pagelets.php
@@ -5,7 +5,7 @@ $ROOT = realpath(dirname(__FILE__)."/../");
 #===============================================================================
 # Enable debugging mode
 #===============================================================================
-BigPipe\BigPipe::debugging(TRUE);
+BigPipe\BigPipe::debugging(FALSE);
 
 #===============================================================================
 # Pagelet: LeftBar
@@ -14,7 +14,7 @@ $LeftBar = new BigPipe\Pagelet('leftbar');
 
 $content = file_get_contents($ROOT.'/static/pagelets/leftbar/leftbar.html', FILE_USE_INCLUDE_PATH);
 $LeftBar->addHTML($content);
-$LeftBar->addCSS('/BigPipe-master/static/pagelets/leftbar/leftbar.css.php');
+$LeftBar->addCSS('/BigPipe/static/pagelets/leftbar/leftbar.css.php');
 
 
 #===============================================================================
@@ -24,7 +24,7 @@ $MiddleBar = new BigPipe\Pagelet('content', BigPipe\Pagelet::PRIORITY_HIGH);
 
 $content = file_get_contents($ROOT.'/static/pagelets/content/content.html', FILE_USE_INCLUDE_PATH);
 $MiddleBar->addHTML($content);
-$MiddleBar->addCSS('/BigPipe-master/static/pagelets/content/content.css.php');
+$MiddleBar->addCSS('/BigPipe/static/pagelets/content/content.css.php');
 
 
 #===============================================================================
@@ -34,7 +34,7 @@ $RightBar = new BigPipe\Pagelet('rightbar', BigPipe\Pagelet::PRIORITY_HIGH);
 
 $content = file_get_contents($ROOT.'/static/pagelets/rightbar/rightbar.html', FILE_USE_INCLUDE_PATH);
 $RightBar->addHTML($content);
-$RightBar->addCSS('/BigPipe-master/static/pagelets/rightbar/rightbar.css.php');
+$RightBar->addCSS('/BigPipe/static/pagelets/rightbar/rightbar.css.php');
 
 
 ?>
