0d8cb053a49725e4f3e047894d268a83948f5ff4
ignace <ignaceagbogba@gmail.com>
2016-12-08 17:50:03 +0100

    compatible pour windows

--- BigPipe/include/pagelets.php
+++ BigPipe/include/pagelets.php
@@ -12,9 +12,9 @@ BigPipe\BigPipe::debugging(FALSE);
 #===============================================================================
 $LeftBar = new BigPipe\Pagelet('leftbar');
 
-$content = file_get_contents($ROOT.'/static/pagelets/leftbar/leftbar.html', FILE_USE_INCLUDE_PATH);
+$content = file_get_contents('C:/wamp/www/BigPipe/BigPipe/static/pagelets/leftbar/leftbar.html', FILE_USE_INCLUDE_PATH);
 $LeftBar->addHTML($content);
-$LeftBar->addCSS('/BigPipe/static/pagelets/leftbar/leftbar.css.php');
+$LeftBar->addCSS('/bigPipe/BigPipe/static/pagelets/leftbar/leftbar.css.php');
 
 
 #===============================================================================
@@ -22,9 +22,9 @@ $LeftBar->addCSS('/BigPipe/static/pagelets/leftbar/leftbar.css.php');
 #===============================================================================
 $MiddleBar = new BigPipe\Pagelet('content', BigPipe\Pagelet::PRIORITY_HIGH);
 
-$content = file_get_contents($ROOT.'/static/pagelets/content/content.html', FILE_USE_INCLUDE_PATH);
+$content = file_get_contents('C:/wamp/www/BigPipe/BigPipe/static/pagelets/content/content.html', FILE_USE_INCLUDE_PATH);
 $MiddleBar->addHTML($content);
-$MiddleBar->addCSS('/BigPipe/static/pagelets/content/content.css.php');
+$MiddleBar->addCSS('/bigPipe/BigPipe/static/pagelets/content/content.css.php');
 
 
 #===============================================================================
@@ -32,9 +32,9 @@ $MiddleBar->addCSS('/BigPipe/static/pagelets/content/content.css.php');
 #===============================================================================
 $RightBar = new BigPipe\Pagelet('rightbar', BigPipe\Pagelet::PRIORITY_HIGH);
 
-$content = file_get_contents($ROOT.'/static/pagelets/rightbar/rightbar.html', FILE_USE_INCLUDE_PATH);
+$content = file_get_contents('C:/wamp/www/BigPipe/BigPipe/static/pagelets/rightbar/rightbar.html', FILE_USE_INCLUDE_PATH);
 $RightBar->addHTML($content);
-$RightBar->addCSS('/BigPipe/static/pagelets/rightbar/rightbar.css.php');
+$RightBar->addCSS('/bigPipe/BigPipe/static/pagelets/rightbar/rightbar.css.php');
 
 
 ?>
